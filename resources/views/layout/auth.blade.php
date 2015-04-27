9af068c1b473e1f9ba9e070e3193ae85d3aeb833
Rasmus <KokholmGames@hotmail.com>
2015-04-27 22:37:32 +0200

    posts migration + more profile styling + responsive profiles

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -6,6 +6,9 @@
 	<link rel="stylesheet" href="css/auth.css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
 <body>
 	
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -9,11 +9,22 @@
 	
 	<div id="content">
 	
-			<div class="profile_banner"></div>
+			<img src="../images/banner.png" class="profile_banner">
 
 			<div class="user-info">
 				 
-
+				<div class="user-info-divide">
+					<i class="fa fa-table profile-icon"></i>Dashboard
+				</div>
+				<div class="user-info-divide">
+					<i class="fa fa-users profile-icon"></i>Friends
+				</div>
+				<div class="user-info-divide">
+					<i class="fa fa-picture-o profile-icon"></i>Images
+				</div>
+				<div class="user-info-divide noborder">
+					<i class="fa fa-info profile-icon"></i>About
+				</div>
 
 			</div>
 
@@ -22,6 +33,28 @@
 			</div>
 
 			<div class="profile-dashboard">
+
+				<div class="post">
+					jkhasbdjahsbdjahsbdjashd
+				</div>
+
+
+				<div class="post">
+					jkhasbdjahsbdjahsbdjashdbjahsbd
+				</div>
+
+
+				<div class="post">
+					jkhasbdjahsbdjahsbdjashdbjahsbd
+				</div>
+
+					{!! Form::open(['route' => 'profile_path']) !!}
+
+						{!! Form::textarea('post-content', null, ['class' => 'new-post', 'placeholder' => 'What is on your mind?...', 'maxlength' => '180']) !!}
+
+						{!! Form::submit('Share', ['class' => 'new-post-submit']) !!}
+
+					{!! Form::close() !!}
 
 			</div>
 
